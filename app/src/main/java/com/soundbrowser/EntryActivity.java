431ecd083c9a3ec0ab899b2bb58b37da82b96a79
Bruno Almeida <brunomarioalmeida@gmail.com>
2014-11-16 22:40:36 +0000

    Main list view from json data

--- app/src/main/java/com/soundbrowser/EntryActivity.java
+++ app/src/main/java/com/soundbrowser/EntryActivity.java
@@ -4,66 +4,83 @@ import android.app.Activity;
 import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.media.ToneGenerator;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.soundbrowser.adapter.CustomAdapter;
+import com.soundbrowser.model.Item;
 import com.soundbrowser.model.SoundSource;
+import com.soundbrowser.model.Track;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
-
 
 public class EntryActivity extends Activity {
 
-    List<String> audiopathList;
-    private int playbackPosition = 0;
-
     private MediaPlayer mediaPlayer;
     private AudioManager am;
+
+    private ListView listView;
+    private  ArrayList<Item> listViewArr = new ArrayList<Item>();
+    private int currentPosition;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_entry);
+
+        currentPosition = 0;
+
         am = (AudioManager)this.getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
         mediaPlayer = new MediaPlayer();
 
-        audiopathList = new ArrayList<String>(){{
-            add("http://users.skynet.be/fa046054/home/P22/track06.mp3");
-            add("http://www.noiseaddicts.com/samples/2558.mp3");
-            add("http://www.noiseaddicts.com/samples/2537.mp3");
-            add("http://www.noiseaddicts.com/samples/2540.mp3");
-        }};
+        listView = (ListView) findViewById(R.id.list);
 
+        /*
         audiopathList = new ArrayList<String>(){{
             add("file://mnt/sdcard/Recording/record20141103222917.3gpp");
-            //add("file://sdcard/recording1831815422.amr");
-
             add("file://mnt/sdcard/Recording/record20141103222947.3gpp");
+
+            // To test in tablet
+            //add("file://sdcard/recording1831815422.amr");
             //add("file://sdcard/recording726735500.amr");
         }};
+        */
 
         try {
             Reader reader = new InputStreamReader(getAssets().open("sounds.json"), "UTF-8");
             Gson gson = new GsonBuilder().create();
             SoundSource p = gson.fromJson(reader, SoundSource.class);
-            audiopathList.add(p.getSource().getSource().getItem().get(0).getTrack().getUrl());
+
+            for (Item itemIn : p.getSource().getSource().getItem())
+            {
+                Track t = new Track();
+                t.setUrl(itemIn.getTrack().getUrl());
+                t.setDuration(itemIn.getTrack().getDuration());
+
+                Item item = new Item();
+                item.setTitle(itemIn.getTitle());
+                item.setTrack(t);
+
+                listViewArr.add(item);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        setContentView(R.layout.activity_entry);
+        CustomAdapter adapter = new CustomAdapter( this, listViewArr, getResources() );
+        listView.setAdapter(adapter);
     }
 
     @Override
@@ -81,7 +98,13 @@ public class EntryActivity extends Activity {
 
         if(keyCode == KeyEvent.KEYCODE_VOLUME_UP ||  keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
             try {
-                playAudio(audiopathList.get(2));
+                if(keyCode == KeyEvent.KEYCODE_VOLUME_UP)
+                    currentPosition++;
+                if(keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)
+                    currentPosition--;
+
+                playAndSchedule(currentPosition);
+
             } catch (Exception e) {
                 Log.i("error : ", e.getLocalizedMessage());
                 e.printStackTrace();
@@ -91,44 +114,34 @@ public class EntryActivity extends Activity {
             return super.dispatchKeyEvent(event);
     }
 
-    public void doClick(View view)
+    // This function used by adapter
+    public void onItemClick(int mPosition)
     {
-        switch(view.getId()) {
-            case R.id.start:
-                playAndSchedule();
-                break;
-/*
-            case R.id.pausePlayerBtn:
-                if(mediaPlayer != null && mediaPlayer.isPlaying()) {
-                    playbackPosition = mediaPlayer.getCurrentPosition();
-                    mediaPlayer.pause();
-                }
-                break;
-            case R.id.restartPlayerBtn:
-                if(mediaPlayer != null && !mediaPlayer.isPlaying()) {
-                    mediaPlayer.seekTo(playbackPosition);
-                    mediaPlayer.start();
-                }
-                break;
-  */
-            case R.id.stop:
-                if(mediaPlayer != null) {
-                    mediaPlayer.stop();
-                    playbackPosition = 0;
-                }
-                break;
-        }
+        listView.getChildAt(mPosition).setBackgroundColor(
+            getResources().getColor(android.R.color.holo_orange_dark)
+        );
+
+        // Show Alert
+        Toast.makeText(
+            getApplicationContext(),
+            " Position : " + mPosition +
+            " ListItem : " + ((TextView)listView.getChildAt(mPosition)).getContentDescription(),
+            Toast.LENGTH_LONG
+        ).show();
+
+        playAndSchedule(mPosition);
     }
 
-    private void playAndSchedule()
+    private void playAndSchedule(int position)
     {
         try {
-            playAudio(audiopathList.get(0));
+            playAudio((( Item ) listViewArr.get(position)).getTrack().getUrl());
         } catch (Exception e) {
             Log.i("error : ", e.getLocalizedMessage());
             e.printStackTrace();
         }
 
+        /* TODO use this later if needed
         TimerTask doAsynchronousTask = new TimerTask() {
             @Override
             public void run()
@@ -148,6 +161,7 @@ public class EntryActivity extends Activity {
         };
         Timer timer = new Timer();
         timer.schedule(doAsynchronousTask, 19000);
+        */
     }
 
     private void playAudio(String url)
@@ -168,7 +182,7 @@ public class EntryActivity extends Activity {
     }
 
     private void killMediaPlayer() {
-        if(mediaPlayer!=null) {
+        if(mediaPlayer != null) {
             try {
                 mediaPlayer.release();
             }
---
+++ app/src/main/java/com/soundbrowser/adapter/CustomAdapter.java
@@ -0,0 +1,123 @@
+package com.soundbrowser.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.soundbrowser.EntryActivity;
+import com.soundbrowser.R;
+import com.soundbrowser.model.Item;
+
+import java.util.ArrayList;
+
+/**
+ * Created by bold on 16-11-2014.
+ */
+public class CustomAdapter extends BaseAdapter implements View.OnClickListener {
+
+    private Activity activity;
+    private ArrayList data;
+    public Resources res;
+    private static LayoutInflater inflater = null;
+
+    Item itemIn;
+    int i = 0;
+
+    public CustomAdapter(Activity a, ArrayList d,Resources resLocal) {
+        activity = a;
+        data = d;
+        res = resLocal;
+
+        // Layout inflator to call external xml layout ()
+        inflater = (LayoutInflater) activity.
+            getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public int getCount() {
+        if(data.size()<=0)
+            return 1;
+        return data.size();
+    }
+
+    public Object getItem(int position) {
+        return position;
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    // Create a holder Class to contain inflated xml file elements
+    public static class ViewHolder {
+        public TextView desc;
+        public TextView duration;
+    }
+
+    // Depends upon data size called for each row
+    // Create each ListView row
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View vi = convertView;
+        ViewHolder holder;
+
+        if(convertView == null)
+        {
+            // Inflate tabitem.xml file for each row ( Defined below )
+            vi = inflater.inflate(R.layout.tabitem, null);
+
+            // View Holder Object to contain tabitem.xml file elements
+            holder = new ViewHolder();
+            holder.desc = (TextView) vi.findViewById(R.id.desc);
+            holder.duration = (TextView)vi.findViewById(R.id.duration);
+            //holder.image=(ImageView)vi.findViewById(R.id.image);
+
+            //Set holder with LayoutInflater
+            vi.setTag( holder );
+        }
+        else
+            holder = (ViewHolder) vi.getTag();
+
+        if(data.size()<=0)
+            holder.desc.setText("No Data");
+        else
+        {
+            // Get each Model object from Arraylist
+            itemIn = (Item) data.get(position);
+
+            // Set Model values in Holder elements
+            holder.desc.setText(itemIn.getTitle());
+            holder.duration.setText(itemIn.getTrack().getDuration());
+
+            // Set Item Click Listner for LayoutInflater for each row
+            vi.setOnClickListener(new OnItemClickListener( position ));
+        }
+
+        return vi;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.v("CustomAdapter", "=====Row button clicked=====");
+    }
+
+    /********* Called when Item click in ListView ************/
+    private class OnItemClickListener implements View.OnClickListener {
+        private int mPosition;
+
+        OnItemClickListener(int position){
+            mPosition = position;
+        }
+
+        @Override
+        public void onClick(View arg0)
+        {
+            EntryActivity sct = (EntryActivity)activity;
+            sct.onItemClick(mPosition);
+        }
+    }
+}
